2e1f56a6b71b408f2760b3cf3cfc37ff9438ca9d
Ivan Bulyk <stackyogi@gmail.com>
2021-05-18 14:48:19 +0300

    Add routing

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/go-chi/chi"
+	"github.com/ivanbulyk/WORKSHOP-VRN/internal/handler"
+)
+
+func main() {
+	h := handler.NewHandler()
+
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ivanbulyk/WORKSHOP-VRN
 
 go 1.16
+
+require github.com/go-chi/chi v1.5.4
