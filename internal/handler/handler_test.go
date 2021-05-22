e342c9ffebc971ad71869900b479f8b4b75b5b3f
Ivan Bulyk <stackyogi@gmail.com>
2021-05-22 17:03:54 +0300

    add test

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -1,33 +1,58 @@
-package handler
+package handler_test
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/ivanbulyk/WORKSHOP-VRN/internal/api"
+	"github.com/ivanbulyk/WORKSHOP-VRN/internal/api/mocks"
+	"github.com/ivanbulyk/WORKSHOP-VRN/internal/handler"
+	// "github.com/ivanbulyk/WORKSHOP-VRN/internal/handler"
 )
 
 func TestHandler_Hello(t *testing.T) {
-	type fields struct {
-		jokeClient api.Client
-	}
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
+
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name     string
+		joke     *api.JokeResponse
+		err      error
+		codeWant int
+		bodyWant string
 	}{
-		// TODO: Add test cases.
+		{
+			name:     "simple test",
+			joke:     &api.JokeResponse{Joke: "test joke"},
+			err:      nil,
+			codeWant: 200,
+			bodyWant: "test joke",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := &Handler{
-				jokeClient: tt.fields.jokeClient,
+			apiMock := &mocks.Client{}
+			apiMock.On("GetJoke").
+				Return(tt.joke, tt.err)
+
+			h := handler.NewHandler(apiMock)
+
+			req, _ := http.NewRequest("GET", "/hello", nil)
+			rr := httptest.NewRecorder()
+
+			h.Hello(rr, req)
+
+			gotRaw, _ := ioutil.ReadAll(rr.Body)
+			got := string(gotRaw)
+
+			if got != tt.bodyWant {
+				t.Errorf("wrong response body %s want %s", got, tt.bodyWant)
 			}
-			h.Hello(tt.args.w, tt.args.r)
+
+			if status := rr.Result().StatusCode; status != tt.codeWant {
+				t.Errorf("wrong response status %d want %d", status, tt.codeWant)
+			}
+
 		})
 	}
 }
